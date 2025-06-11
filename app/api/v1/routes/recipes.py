53069a6b3a02f234b93c83f0b5cbfdf3e4c12412
jsamuelsen11 <jsamuelsen11@gmail.com>
2025-06-11 00:30:29 -0400

    Implemented create recipe endpoint (#20)
    * Project config updates.
    
    * Database model enhancements.
    
    * Schema model updates.
    
    * Implemented create recipe endpoint.

--- app/api/v1/routes/recipes.py
+++ app/api/v1/routes/recipes.py
@@ -5,17 +5,21 @@ popular links.
 """
 
 from typing import Annotated
+from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 from fastapi.params import Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
 
 from app.api.v1.schemas.common.pagination_params import PaginationParams
 from app.api.v1.schemas.request.create_recipe_request import CreateRecipeRequest
+from app.api.v1.schemas.response.create_recipe_response import CreateRecipeResponse
 from app.api.v1.schemas.response.recommended_recipes_response import (
     PopularRecipesResponse,
 )
 from app.core.logging import get_logger
+from app.deps.db import get_db
 from app.services.recipe_scraper_service import RecipeScraperService
 
 __log = get_logger("RecipeScraperRoutes")
@@ -42,19 +46,22 @@ router = APIRouter()
 )
 def create_recipe(
     service: Annotated[RecipeScraperService, Depends(get_recipe_scraper_service)],
+    db: Annotated[Session, Depends(get_db)],
     request: CreateRecipeRequest,
-) -> JSONResponse:
-    """Endpoint to extract a recipe from a given URL.
+    user_id: Annotated[UUID, Header(..., alias="X-User-ID")],
+) -> CreateRecipeResponse:
+    """Endpoint to create a recipe from a given URL and add it to the database.
 
     Args:
-        service (RecipeScraperService): RecipeScraperService dependency for processing
-            the recipe.
+        service (RecipeScraperService): Service for recipe scraping logic.
+        db (Session): Database session dependency.
         request (CreateRecipeRequest): Request body containing the recipe URL.
+        user_id (UUID): The unique identifier of the user making the request.
 
     Returns:
-        JSONResponse: Response containing the created recipe data or error details.
+        CreateRecipeResponse: The created recipe response.
     """
-    return service.create_recipe(request.recipe_url)
+    return service.create_recipe(request.recipe_url, db, user_id=user_id)
 
 
 @router.get(
--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ routers, and other startup procedures.
 from fastapi import FastAPI
 
 from app.api.v1.routes import api_router
+from app.exceptions.handlers import unhandled_exception_handler
 from app.middleware.request_id_middleware import RequestIDMiddleware
 
 app = FastAPI(
@@ -14,5 +15,6 @@ app = FastAPI(
     version="1.0.0",
     description="An API for scraping and managing recipe data.",
 )
+app.add_exception_handler(Exception, unhandled_exception_handler)
 app.add_middleware(RequestIDMiddleware)
 app.include_router(api_router, prefix="/api")
