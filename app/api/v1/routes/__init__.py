b74d11292983b99aa1c18a4ad407711ed4df75fd
jsamuelsen <jsamuelsen11@gmail.com>
2025-06-27 22:32:14 -0400

    Added admin resource to clear cache.

--- app/api/v1/routes/__init__.py
+++ app/api/v1/routes/__init__.py
@@ -5,11 +5,10 @@ Groups all route modules for API version 1.
 
 from fastapi import APIRouter
 
-from app.api.v1.routes import recommendations
-
-from . import health, nutritional_info, recipes
+from . import admin, health, nutritional_info, recipes, recommendations
 
 api_router = APIRouter()
+api_router.include_router(admin.router)
 api_router.include_router(health.router)
 api_router.include_router(nutritional_info.router)
 api_router.include_router(recipes.router)
